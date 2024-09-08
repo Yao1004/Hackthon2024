22b1ce98489168c920d260b80f93ef5c37c93528
unknown <changmingyao2014@gmail.com>
2024-09-08 08:10:54 +0800

    modify url

--- lib/page/city_service/model/trending_service_model.dart
+++ lib/page/city_service/model/trending_service_model.dart
@@ -5,11 +5,6 @@ abstract final class TrendingServiceModel {
   static List<TrendingService> get serviceList {
     return [
       TrendingService(
-        title: '找地點',
-        icon: Assets.svg.iconLocationSearch.svg(),
-        url: 'https://taipei-pass-service.vercel.app/surrounding-service/',
-      ),
-      TrendingService(
         icon: Assets.svg.iconDashboardReports.svg(),
         title: '市民儀表板',
         url: 'https://dashboard.gov.taipei/',
@@ -17,8 +12,8 @@ abstract final class TrendingServiceModel {
       TrendingService(
         icon: Assets.svg.iconLocationSearch.svg(),
         title: '降雨報報',
-        url: 'http://10.0.2.2:54088/',
-        //url: 'https://4da9-211-23-28-236.ngrok-free.app/',
+        //url: 'http://10.0.2.2:54088/',
+        url: 'https://b975-211-23-28-236.ngrok-free.app'
       ),
       // 在此列表後加入新熱門按鈕
       // add new trending service button here
